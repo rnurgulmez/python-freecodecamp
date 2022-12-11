e432c9e714ca2dc3dd87f21c752168d909d3cfd9
Rabia Nur Gülmez <99583312+rabianurg8@users.noreply.github.com>
2022-12-11 12:21:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -190,3 +190,29 @@ monthConversions = {
 print(monthConversions["Apr"])
 print(monthConversions.get("Apr")) # 
 print(monthConversions.get("Luv","Not a valid key"))
+
+# while loop
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+print("Done with loop")
+
+# building a guessing game
+secret_word = "giraffe"
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
+
+while guess != secret_word and  not(out_of_guesses):
+    if guess_count < guess_limit:
+        guess = input("Enter guess : ")
+        guess_count += 1
+    else:
+        out_of_guesses = True
+
+if out_of_guesses:
+    print("Out of guesses, YOU LOSE")
+else:
+    print("You win!")
