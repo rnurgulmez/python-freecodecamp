d8cf55068df3918442991e5672e6e51b9b2a1d9c
Rabia Nur Gülmez <99583312+rabianurg8@users.noreply.github.com>
2023-01-07 12:09:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -392,3 +392,38 @@ student2 = Student("omer","engineer",1.1,True)
 
 print(student2.gpa)
 print(student1.name)
+
+# building a multiple choice quiz
+
+#Question.py
+class Question:
+
+    def __init__(self, prompt, answer):
+        self.prompt = prompt
+        self.answer = answer
+        
+# app.py
+from Question import Question
+
+question_prompts = [
+    "What color are apples?\n(a) Red/Green\n(b) Purple\n (c) Orange\n\n" ,
+    "What color are bananas?\n(a) Teal\n(b) Magenta\n (c) Yellow\n\n",
+    "What color are strawberries?\n(a) Yellow\n(b) Red\n (c) Blue\n\n"
+]
+
+questions = [
+    Question(question_prompts[0], "a"),
+    Question(question_prompts[1], "c"),
+    Question(question_prompts[2], "b")
+]
+
+def run_test(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
+
+    print("You got " + str(score) + "/" + str(len(questions)) + " correct")
+
+run_test(questions)
