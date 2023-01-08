d159291c9aaeaa74d94af8ec59c5167f282ffdef
Rabia Nur Gülmez <99583312+rabianurg8@users.noreply.github.com>
2023-01-08 05:01:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -427,3 +427,38 @@ def run_test(questions):
     print("You got " + str(score) + "/" + str(len(questions)) + " correct")
 
 run_test(questions)
+
+# Inheritance
+#Chef.py
+class Chef:
+    def make_chicken(self):
+        print("The chef makes a chicken")
+
+    def make_salad(self):
+        print("The chef makes a salad")
+
+    def make_special_dish(self):
+        print("The chef makes a bbq ribs")
+        
+#ChineseChef.py
+from Chef import Chef
+
+class ChineseChef(Chef):
+
+    def make_special_dish(self):                   #override
+        print("The chef makes orange chicken")
+
+    def make_fried_rice(self):
+        print("The chef makes fried rice")
+       
+#app.py
+from Chef import Chef
+from ChineseChef import ChineseChef
+
+myChef = Chef()
+
+myChef.make_special_dish()
+
+myChineseChef = ChineseChef()
+
+myChineseChef.make_special_dish()
